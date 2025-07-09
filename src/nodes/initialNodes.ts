0481bbe86083d1768a3c20f15acd83fb5ca56201
Chadwick <hello@chadwickmacmillan.com>
2025-07-09 13:17:51 -0400

    fix: remove default nodes

--- src/nodes/initialNodes.ts
+++ src/nodes/initialNodes.ts
@@ -1,23 +1,3 @@
 import type { AppNode } from "./types";
 
-export const initialNodes: AppNode[] = [
-  { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
-  {
-    id: "b",
-    type: "position-logger",
-    position: { x: -100, y: 100 },
-    data: { label: "drag me!" },
-  },
-  {
-    id: "c",
-    type: "default",
-    position: { x: 100, y: 100 },
-    data: { label: "your ideas" },
-  },
-  {
-    id: "d",
-    type: "output",
-    position: { x: 0, y: 200 },
-    data: { label: "with React Flow" },
-  },
-];
+export const initialNodes: AppNode[] = [];
