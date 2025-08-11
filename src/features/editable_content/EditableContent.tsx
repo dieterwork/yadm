67f8fca9f5140605bfcb89b79f3aee7b3c7c6c61
Chadwick <hello@chadwickmacmillan.com>
2025-08-11 08:20:12 -0400

    fix: add input type for single lines

--- src/features/editable_content/EditableContent.tsx
+++ src/features/editable_content/EditableContent.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@sglara/cn";
 import type { HTMLAttributes } from "react";
+import { Input, TextField } from "react-aria-components";
+import { useDEMOModeler } from "../modeler/useDEMOModeler";
+import { useShallow } from "zustand/react/shallow";
+import { useNodeId } from "@xyflow/react";
 
 type TextPosition = "top" | "bottom" | "center";
 
@@ -7,9 +11,13 @@ interface EditableContentProps
   extends Omit<HTMLAttributes<HTMLDivElement>, "content"> {
   width?: number;
   height?: number;
-  content?: string[];
+  content?: string[] | string;
   editable?: boolean;
   textPosition?: TextPosition;
+  as?: "contentEditable" | "input";
+  maxLength?: number;
+  size?: "small" | "medium" | "large";
+  color?: string;
 }
 
 const getAlignContentStyle = (textPosition: TextPosition) => {
@@ -29,37 +37,111 @@ const getAlignContentStyle = (textPosition: TextPosition) => {
   }
 };
 
+const getPadding = (size: string) => {
+  switch (size) {
+    case "small": {
+      return "p-6";
+    }
+    case "medium": {
+      return "p-4";
+    }
+    case "large": {
+      return "p-2";
+    }
+    default: {
+      throw new Error("Invalid size");
+    }
+  }
+};
+
+const getFontSize = (size: string) => {
+  switch (size) {
+    case "small": {
+      return "text-sm";
+    }
+    case "medium": {
+      return "text-base";
+    }
+    case "large": {
+      return "text-lg";
+    }
+    default: {
+      throw new Error("Invalid size");
+    }
+  }
+};
+
 const EditableContent = ({
   width,
   height,
   content,
   editable = true,
   textPosition = "center",
+  as = "contentEditable",
+  size = "medium",
+  maxLength = 2,
+  color = "var(--color-black)",
   ...restProps
 }: EditableContentProps) => {
   const classes = cn(
-    "editable-content-wrapper | absolute inset-0 m-auto p-4 overflow-hidden text-center",
-    { "w-full": !width, "h-full": !height }
+    "editable-content-wrapper | absolute inset-0 m-auto overflow-hidden text-center",
+    { "w-full": !width, "h-full": !height },
+    getPadding(size),
+    getFontSize(size)
   );
+
+  const nodeId = useNodeId();
+
+  const { updateNodeContent, getNode } = useDEMOModeler(
+    useShallow((state) => ({
+      updateNodeContent: state.updateNodeContent,
+      getNode: state.getNode,
+    }))
+  );
+
   return (
     <div
       {...restProps}
       className={cn(classes, restProps.className)}
-      style={{ ...restProps.style, width, height }}
+      style={{ ...restProps.style, width, height, color }}
     >
-      <div
-        contentEditable={!!editable}
-        suppressContentEditableWarning={true}
-        className="block w-full h-full break-all overflow-hidden focus-visible:outline-none whitespace-pre-wrap content-not-editable:select-none"
-        style={{ alignContent: getAlignContentStyle(textPosition) }}
-      >
-        {content &&
-          content.map((text) => (
+      {as === "contentEditable" ? (
+        <div
+          contentEditable={!!editable}
+          suppressContentEditableWarning={true}
+          className="block w-full h-full break-all overflow-hidden focus-visible:outline-none whitespace-pre-wrap content-not-editable:select-none"
+          style={{ alignContent: getAlignContentStyle(textPosition) }}
+        >
+          {typeof content === "string" ? (
             <>
-              <div>{text}</div>
+              <div>{content}</div>
             </>
-          ))}
-      </div>
+          ) : (
+            Array.isArray(content) &&
+            content.map((text) => (
+              <>
+                <div>{text}</div>
+              </>
+            ))
+          )}
+        </div>
+      ) : (
+        typeof content === "string" && (
+          <TextField
+            className="block w-full h-full break-all overflow-hidden focus-visible:outline-none whitespace-pre-wrap content-not-editable:select-none"
+            onChange={(val) => {
+              updateNodeContent(nodeId ?? "", val);
+            }}
+            defaultValue={content}
+            maxLength={maxLength}
+          >
+            <Input
+              className="block w-full h-full"
+              style={{ textAlign: getAlignContentStyle(textPosition) }}
+            />
+          </TextField>
+        )
+      )}
     </div>
   );
 };
