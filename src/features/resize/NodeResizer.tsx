373dd7f63c5831bbbfca1280f594cd4da23ba772
Chadwick <hello@chadwickmacmillan.com>
2025-08-11 08:20:40 -0400

    feat: TODO add custom node resizer

---
+++ src/features/resize/NodeResizer.tsx
@@ -0,0 +1,23 @@
+const NodeResizer = () => {
+  const { getNode, getChildrenNodes, updateNodeExtent } = useDEMOModeler(
+    useShallow((state) => ({
+      getNode: state.getNode,
+      getChildrenNodes: state.getChildrenNodes,
+      updateNodeExtent: state.updateNodeExtent,
+    }))
+  );
+
+  const changeChildExtent = () => {
+    const parentNode = getNode(id);
+    const childrenNodes = getChildrenNodes(id);
+
+    for (const child of childrenNodes) {
+      if (!child || !child?.extent || child?.extent === "parent") return;
+      updateNodeExtent(child.id, [
+        [12.5, 12.5],
+        [parentNode.width - 12.5, parentNode.height - 12.5],
+      ]);
+    }
+  };
+};
+export default NodeResizer;
