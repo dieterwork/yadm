7640fa056131bde4f3c515afaeffc6a89d2627d2
Chadwick <hello@chadwickmacmillan.com>
2025-08-15 11:11:21 -0400

    side menu updates

--- src/features/side_menu/SideMenu.tsx
+++ src/features/side_menu/SideMenu.tsx
@@ -1,9 +1,9 @@
 import { Button, Menu, MenuItem, MenuTrigger } from "react-aria-components";
-import { TextAaIcon } from "@phosphor-icons/react";
+import { FilePlusIcon, TextAaIcon } from "@phosphor-icons/react";
 import { useDEMOModeler } from "../modeler/useDEMOModeler";
 import { useShallow } from "zustand/react/shallow";
 import { usePreviewNode } from "../sidebar/usePreviewNode";
-import { DEFAULT_CONTENT_MAP } from "../nodes/utils/consts";
+import { DEFAULT_CONTENT_MAP, DEFAULT_SIZE_MAP } from "../nodes/utils/consts";
 
 const SideMenu = () => {
   const { addNode } = useDEMOModeler(
@@ -18,14 +18,13 @@ const SideMenu = () => {
     {
       id: "add_text_node",
       label: "Add text",
-      icon: TextAaIcon,
+      icon: FilePlusIcon,
       action: () => {},
       onClick: (e: React.MouseEvent) => {
-        //
         createNode({
           type: "text_node",
-          width: 100,
-          height: 100,
+          width: DEFAULT_SIZE_MAP["text_node"].width,
+          height: DEFAULT_SIZE_MAP["text_node"].height,
           position: { x: e.clientX, y: e.clientY },
           content: DEFAULT_CONTENT_MAP["text_node"],
         });
@@ -33,18 +32,23 @@ const SideMenu = () => {
     },
   ];
   return (
-    <div className="sidemenu | absolute top-[50%] left-4 translate-y-[-50%]">
+    <div className="sidemenu | absolute top-[50%] left-4 translate-y-[-50%] z-9999">
       <div className="sidemenu-inner">
-        <Menu items={sideMenuItems}>
-          {(sideMenuItem) => (
-            <MenuItem
-              key={sideMenuItem.id}
-              onClick={sideMenuItem.onClick}
-              className="cursor-pointer select-none"
-            >
-              {sideMenuItem.label}
-            </MenuItem>
-          )}
+        <Menu items={sideMenuItems} className="bg-white">
+          <MenuItem
+            onClick={(e) => {
+              createNode({
+                type: "text_node",
+                width: 100,
+                height: 100,
+                position: { x: e.clientX, y: e.clientY },
+                content: DEFAULT_CONTENT_MAP["text_node"],
+              });
+            }}
+            className="cursor-pointer select-none"
+          >
+            <FilePlusIcon size={32} />
+          </MenuItem>
         </Menu>
       </div>
     </div>
