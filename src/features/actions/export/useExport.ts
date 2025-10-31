d5935ef0d6872f4f115b53ddf09839ec142d8fc3
Chadwick <hello@chadwickmacmillan.com>
2025-10-31 00:38:42 -0400

    updates

--- src/features/actions/export/useExport.ts
+++ src/features/actions/export/useExport.ts
@@ -8,6 +8,27 @@ import { downloadFile, generatePNG } from "./utils";
 import "svg2pdf.js";
 import { useReactFlow } from "@xyflow/react";
 import type { DEMOModelJSON } from "$/shared/types/reactFlow.types";
+import { z } from "zod";
+import toast from "react-hot-toast/headless";
+
+const fileNameSchema = z
+  .string()
+  .max(80, { error: "File name must be less than 80 characters" })
+  .regex(new RegExp(/([A-Za-z0-9\-\_]+)/), {
+    error:
+      "File name can only contain letters, numbers, parentheses, underscores, and dashes",
+  });
+
+const testFileName = (fileName: string) => {
+  try {
+    fileNameSchema.parse(fileName);
+    return { success: true };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return { success: false, error: error };
+    }
+  }
+};
 
 const useExport = () => {
   const nodes = useDEMOModelerStore((state) => state.nodes);
@@ -19,17 +40,24 @@ const useExport = () => {
 
   const exportAsPNG = async (scaleFactor: number) => {
     try {
+      const parsedFileName = fileNameSchema.parse(fileName);
       setExportEnabled(true);
       const { url } = await generatePNG({ nodesBounds, scaleFactor });
-      downloadFile(url, fileName + ` x${scaleFactor}.png`);
+      downloadFile(url, parsedFileName + ` x${scaleFactor}.png`);
       setExportEnabled(false);
     } catch (err) {
       console.error("Could not generate PNG.", err);
+      if (err instanceof z.ZodError) {
+        for (const error of err.issues) {
+          toast.error(error.message);
+        }
+      }
     }
   };
 
   const exportAsPDF = async (scaleFactor: number) => {
     try {
+      const parsedFileName = fileNameSchema.parse(fileName);
       setExportEnabled(true);
       const { url, width, height } = await generatePNG({
         nodesBounds,
@@ -38,25 +66,40 @@ const useExport = () => {
       const doc = new jsPDF("landscape", "px", [width, height]);
       doc.addImage(url, "png", width / 4, height / 4, width / 2, height / 2);
       doc.output("dataurlnewwindow", {
-        filename: fileName + ` x${scaleFactor}.pdf`,
+        filename: parsedFileName + ` x${scaleFactor}.pdf`,
       });
       setExportEnabled(false);
     } catch (err) {
       console.error("Could not generate PDF.", err);
+      if (err instanceof z.ZodError) {
+        for (const error of err.issues) {
+          toast.error(error.message);
+        }
+      }
     }
   };
 
   const exportAsJSON = () => {
     if (!DEMOInstance) return;
-    const jsModel: DEMOModelJSON = {
-      ...DEMOInstance.toObject(),
-      isEnabled,
-      version: "1.0.0",
-    };
-    const jsonModel = JSON.stringify(jsModel);
-    const file = new Blob([jsonModel], { type: "application/json" });
-    const url = URL.createObjectURL(file);
-    downloadFile(url, fileName + ".json");
+    try {
+      const parsedFileName = fileNameSchema.parse(fileName);
+      const jsModel: DEMOModelJSON = {
+        ...DEMOInstance.toObject(),
+        isEnabled,
+        version: "1.0.0",
+      };
+      const jsonModel = JSON.stringify(jsModel);
+      const file = new Blob([jsonModel], { type: "application/json" });
+      const url = URL.createObjectURL(file);
+      downloadFile(url, parsedFileName + ".json");
+    } catch (err) {
+      console.error("Could not generate JSON:", err);
+      if (err instanceof z.ZodError) {
+        for (const error of err.issues) {
+          toast.error(error.message);
+        }
+      }
+    }
   };
 
   return { exportAsPNG, exportAsPDF, exportAsJSON };
--- src/features/actions/save/useSave.ts
+++ src/features/actions/save/useSave.ts
@@ -3,20 +3,12 @@ import type { DEMOModelJSON } from "$/shared/types/reactFlow.types";
 import debounce from "$/shared/utils/debounce";
 import { z } from "zod";
 
-const fileNameSchema = z
-  .string()
-  .max(80, { error: "File name must be less than 80 characters" })
-  .regex(new RegExp(/([A-Za-z0-9\-\_]+)/), {
-    error:
-      "File name can only contain letters, numbers, parentheses, underscores, and dashes",
-  });
-
 const useSave = () => {
   const DEMOInstance = useDEMOModelerStore((state) => state.DEMOInstance);
   const isEnabled = useDEMOModelerStore((state) => state.isEnabled);
   const fileName = useDEMOModelerStore((state) => state.fileName);
 
-  const saveDEMOModel = () => {
+  const save = () => {
     if (!DEMOInstance) return;
     const jsonModel = JSON.stringify({
       ...DEMOInstance.toObject(),
@@ -26,21 +18,8 @@ const useSave = () => {
     localStorage.setItem("demo-model", jsonModel);
   };
 
-  const save = () => {
-    try {
-      if (!DEMOInstance) return;
-      fileNameSchema.parse(fileName);
-      saveDEMOModel();
-      return { success: true };
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return { success: false, error: error };
-      }
-    }
-  };
-
   const autoSave = debounce(() => {
-    saveDEMOModel();
+    save();
   }, 3000);
 
   return { save, autoSave };
