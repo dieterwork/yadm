86c1bc479829aa2025f88498af8a36e9338c1207
Chadwick <hello@chadwickmacmillan.com>
2026-03-25 15:12:53 -0400

    server update

--- src/shared/components/ui/modal/ServerPasswordModal.tsx
+++ src/shared/components/ui/modal/ServerPasswordModal.tsx
@@ -38,28 +38,27 @@ const ServerPasswordModal = ({
 
   return (
     <TopbarMenuModal {...restProps}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex items-center justify-between"
-      >
-        <TextField className="grid grid-cols-[auto_1fr] items-center gap-2">
-          <Label className="text-slate-900 text-xs">
-            {t(($) => $["password"])}
-          </Label>
-          <Input
-            type="password"
-            {...register("password", { required: true })}
-            className="outline-hidden border-1 border-slate-200 rounded-sm w-[16rem] text-sm h-[2rem] content-center px-2 focus-within:ring-2 focus-within:ring-sky-500"
-          />
-          {errors.password && <p role="alert">{errors.password.message}</p>}
-        </TextField>
-        <Button
-          type="submit"
-          className="inline-flex items-center gap-2 cursor-pointer rounded bg-sky-600 px-4 py-2 text-sm text-white data-hover:bg-sky-500 data-hover:data-active:bg-sky-700"
-        >
-          Save
-          {isPending && <CircleNotchIcon className="animate-spin" />}
-        </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section className="flex items-center justify-between">
+          <TextField className="grid grid-cols-[auto_1fr] items-center gap-2">
+            <Label className="text-slate-900 text-xs">
+              {t(($) => $["password"])}
+            </Label>
+            <Input
+              type="password"
+              {...register("password", { required: true })}
+              className="outline-hidden border-1 border-slate-200 rounded-sm w-[16rem] text-sm h-[2rem] content-center px-2 focus-within:ring-2 focus-within:ring-sky-500"
+            />
+            {errors.password && <p role="alert">{errors.password.message}</p>}
+          </TextField>
+          <Button
+            type="submit"
+            className="inline-flex items-center gap-2 cursor-pointer rounded bg-sky-600 px-4 py-2 text-sm text-white data-hover:bg-sky-500 data-hover:data-active:bg-sky-700"
+          >
+            Save
+            {isPending && <CircleNotchIcon className="animate-spin" />}
+          </Button>
+        </section>
         {errorMessage && <p role="alert">{errorMessage}</p>}
       </form>
     </TopbarMenuModal>
