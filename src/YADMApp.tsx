3f574e46f0b6680e7c7329b87f6d82fac4d0ed97
Chadwick <hello@chadwickmacmillan.com>
2026-04-10 12:25:10 -0400

    fix: move url check to top level of app

--- src/YADMApp.tsx
+++ src/YADMApp.tsx
@@ -1,14 +1,130 @@
 import "@xyflow/react/dist/style.css";
 import "./index.css";
 import AppLayout from "./shared/components/layout/AppLayout";
-import { ReactFlowProvider } from "@xyflow/react";
-import { Suspense } from "react";
+import { ReactFlowProvider, useReactFlow } from "@xyflow/react";
+import { Suspense, useEffect, useId, useState } from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+} from "@tanstack/react-query";
+import toast from "react-hot-toast/headless";
+import { useTranslation } from "react-i18next";
+import loadPublicModel from "./features/actions/load/loadPublicModel";
+import useSharedServerModel from "./features/modeler/hooks/useSharedServerModel";
+import { setModel } from "./features/modeler/store/useDEMOModelerStore";
+import type { DEMOModelJSON } from "./shared/types/reactFlow.types";
+import type { AppError } from "./shared/utils/AppError";
+import loadServerModel from "./features/actions/load/loadServerModel";
+import ServerPasswordModal from "./shared/components/ui/modal/ServerPasswordModal";
 
 const queryClient = new QueryClient();
 
 const YADMApp = () => {
+  const { fitView } = useReactFlow();
+  const [isSharedModel, setSharedModel] = useSharedServerModel();
+  const [isPwdModalOpen, setPwdModalOpen] = useState(false);
+  const loadingId = useId();
+  const { t } = useTranslation();
+
+  const publicModelMutation = useMutation({
+    mutationKey: ["public_model_test"],
+    mutationFn: loadPublicModel,
+    onSuccess: (data) => {
+      toast.dismiss(loadingId);
+      toast.success(
+        t(($) => $["Loaded model"], {
+          fileName: data.fileName,
+        })
+      );
+      setModel({ ...data, isEnabled: false });
+      fitView();
+    },
+    onMutate: () => {
+      toast.loading(
+        t(($) => $["Loading model"]),
+        { id: loadingId }
+      );
+    },
+    onError: () => {
+      toast.dismiss(loadingId);
+      toast.error(t(($) => $["Error loading model"]));
+    },
+  });
+
+  const serverModelMutation = useMutation<
+    DEMOModelJSON,
+    AppError,
+    string,
+    void
+  >({
+    mutationKey: ["server_model"],
+    mutationFn: loadServerModel,
+    onSuccess: (data) => {
+      if (isPwdModalOpen) {
+        setPwdModalOpen(false);
+      }
+      toast.dismiss(loadingId);
+      toast.success(
+        t(($) => $["Loaded model"], {
+          fileName: data.fileName,
+        })
+      );
+      setModel(data);
+      fitView();
+    },
+    onMutate: () => {
+      toast.loading(
+        t(($) => $["Loading model"]),
+        { id: loadingId }
+      );
+    },
+    onError: (error) => {
+      toast.dismiss(loadingId);
+      if (error.httpCode === 401) {
+        setPwdModalOpen(true);
+      } else {
+        toast.error(t(($) => $["Error loading model. Please try again."]));
+      }
+    },
+  });
+
+  useEffect(() => {
+    const modelName =
+      new URLSearchParams(window.location.search).get("model") ?? "";
+
+    if (modelName !== "" && modelName.includes("/")) {
+      const piecesCount = modelName.split("/").length;
+
+      console.log(modelName);
+
+      if (piecesCount === 3) {
+        // 3 slashes is my models
+        const [mymodels, , fileName] = modelName.split("/");
+
+        if (mymodels === "mymodels") {
+          console.log("my models");
+          serverModelMutation.mutate(fileName);
+          setSharedModel(true);
+        }
+      } else if (piecesCount === 2) {
+        // 2 slashes is a public model
+
+        console.log("public model");
+
+        const [company, fileName] = modelName.split("/");
+
+        publicModelMutation.mutate({ fileName, company });
+        setSharedModel(true);
+      } else {
+        setSharedModel(false);
+      }
+    } else {
+      setSharedModel(false);
+    }
+  }, []);
+
   return (
     <div className="yadm-app | h-[100svh]">
       <Suspense fallback="loading">
@@ -18,6 +134,16 @@ const YADMApp = () => {
           </ReactFlowProvider>
         </QueryClientProvider>
       </Suspense>
+      <ServerPasswordModal
+        isOpen={isPwdModalOpen}
+        onOpenChange={(isOpen) => setPwdModalOpen(isOpen)}
+        isPending={serverModelMutation.isPending}
+        errorMessage={
+          serverModelMutation.error?.httpCode === 401
+            ? "Invalid password"
+            : undefined
+        }
+      />
     </div>
   );
 };
--- src/shared/components/layout/topbar/menus/ServerModelsMenu.tsx
+++ src/shared/components/layout/topbar/menus/ServerModelsMenu.tsx
@@ -1,11 +1,11 @@
 import TopbarMenuButton from "../_components/TopbarMenuButton";
 import TopbarMenuItem from "../_components/TopbarMenuItem";
 import { useTranslation } from "react-i18next";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useId, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useId, useState } from "react";
 import loadServerModels from "$/features/actions/load/loadServerModels";
 import loadServerModel from "$/features/actions/load/loadServerModel";
-import toast, { useToaster } from "react-hot-toast/headless";
+import toast from "react-hot-toast/headless";
 import ServerPasswordModal from "$/shared/components/ui/modal/ServerPasswordModal";
 import useUserStore from "$/features/auth/useUserStore";
 import type { AppError } from "$/shared/utils/AppError";
@@ -14,7 +14,6 @@ import { setModel } from "$/features/modeler/store/useDEMOModelerStore";
 import TopbarMenuItemLoadingState from "../_components/TopbarMenuItemLoadingState";
 import TopbarMenuItemErrorState from "../_components/TopbarMenuItemErrorState";
 import useSharedServerModel from "$/features/modeler/hooks/useSharedServerModel";
-import loadPublicModel from "$/features/actions/load/loadPublicModel";
 import { useReactFlow } from "@xyflow/react";
 import TopbarMenuButtonAutoComplete from "../_components/TopbarMenuButtonAutoComplete";
 import uuid from "$/shared/utils/uuid";
@@ -77,71 +76,7 @@ const ServerModelsMenu = () => {
     },
   });
 
-  const publicModelMutation = useMutation({
-    mutationKey: ["public_model_test"],
-    mutationFn: loadPublicModel,
-    onSuccess: (data) => {
-      toast.dismiss(loadingId);
-      toast.success(
-        t(($) => $["Loaded model"], {
-          fileName: data.fileName,
-        })
-      );
-      setModel({ ...data, isEnabled: false });
-      fitView();
-    },
-    onMutate: () => {
-      toast.loading(
-        t(($) => $["Loading model"]),
-        { id: loadingId }
-      );
-    },
-    onError: () => {
-      toast.dismiss(loadingId);
-      toast.error(t(($) => $["Error loading model"]));
-    },
-  });
-
-  useEffect(() => {
-    const modelName =
-      new URLSearchParams(window.location.search).get("model") ?? "";
-
-    if (modelName !== "" && modelName.includes("/")) {
-      const piecesCount = modelName.split("/").length;
-
-      console.log(modelName);
-
-      if (piecesCount === 3) {
-        // 3 slashes is my models
-        const [mymodels, , fileName] = modelName.split("/");
-
-        if (mymodels === "mymodels") {
-          console.log("my models");
-          serverModelMutation.mutate(fileName);
-          setSharedModel(true);
-        }
-      } else if (piecesCount === 2) {
-        // 2 slashes is a public model
-
-        console.log("public model");
-
-        const [company, fileName] = modelName.split("/");
-
-        publicModelMutation.mutate({ fileName, company });
-        setSharedModel(true);
-      } else {
-        setSharedModel(false);
-      }
-    } else {
-      setSharedModel(false);
-    }
-  }, []);
-
   const label = t(($) => $["My models"]);
-
-  useEffect(() => {
-    console.log(searchValue, serverModelsQuery.data);
-  }, [searchValue, serverModelsQuery.data]);
 
   if (serverModelsQuery.isError || !serverModelsQuery.data) {
     return (
