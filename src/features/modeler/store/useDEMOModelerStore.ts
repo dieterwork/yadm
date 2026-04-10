53f0bb2ab25e665ca5cdefece6694073594de189
Chadwick <hello@chadwickmacmillan.com>
2026-04-10 12:59:56 -0400

    fix lock toggle

--- src/features/modeler/store/useDEMOModelerStore.ts
+++ src/features/modeler/store/useDEMOModelerStore.ts
@@ -590,7 +590,7 @@ export const setModel = (model: DEMOModelJSON) => {
   setNodes(model.nodes.map((node) => ({ ...node, selected: false })));
   setEdges(model.edges.map((edge) => ({ ...edge, selected: false })));
   setFileName(model.fileName);
-  setEnabled(model.isEnabled ?? true);
+  toggleLock(model.isEnabled ?? true);
   setViewport(model.viewport ?? { x: 0, y: 0, zoom: 1 });
 };
 
